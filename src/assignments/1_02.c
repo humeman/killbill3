817f63b7462b3f770a8f6fd909a9b4cabcb1ebd3
humeman <camdenjsf@gmail.com>
2025-02-06 12:03:47 -0600

    Works now

--- src/assignments/1_02.c
+++ src/assignments/1_02.c
@@ -13,10 +13,12 @@
 #define ROOM_MIN_HEIGHT 3
 #define ROOM_MAX_RANDOMNESS 6
 
-#define DEFAULT_PATH "/.cs327/dungeon"
+#define DEFAULT_PATH "/.rlg327/dungeon"
+
+int prepare_args(int argc, char* argv[], int *draw_border, int *read, int *write, char *path);
 
 int main(int argc, char* argv[]) {
-    int x, y, i;
+    int x, y;
     dungeon dungeon;
 
     srand(time(NULL));
@@ -24,43 +26,8 @@ int main(int argc, char* argv[]) {
     int draw_border = 0; 
     int read = 0;
     int write = 0;
-    int path_next = 0;
-    char* home = getenv("HOME");
-    char* path = malloc(sizeof (char) * (strlen(home) + strlen(DEFAULT_PATH)) + 1);
-    strcpy(path, home);
-    strcat(path, DEFAULT_PATH);
-    for (i = 1; i < argc; i++) {
-        if (path_next) {
-            free(path);
-            path = argv[i];
-            path_next = 0;
-        }
-        else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--load") == 0) read = 1;
-        else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--save") == 0) write = 1;
-        else if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--border") == 0) draw_border = 1;
-        else if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--path") == 0) path_next = 1;
-        else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-            printf("usage: %s [-lwbph]\n  -l/--load: load a rungeon\n  -s/--save: write a random dungeon\n", argv[0]);
-            printf("  -b/--border: draw a border around the dungeon\n  -p/--path: override path to load/save ");
-            printf("(default ~/.cs327/dungeon)\n  -h/--help: display this message\n");
-            free(path);
-            return 0;
-        }
-        else {
-            fprintf(stderr, "err: unrecognized argument: %s. run -h/--help for usage\n", argv[i]);
-            free(path);
-            return 1;
-        }
-    }
-    if (path_next) {
-        fprintf(stderr, "err: specify a path after -p/--path\n");
-        free(path);
-        return 1;
-    }
-
-    if ((!read && !write) || (read && write)) {
-        fprintf(stderr, "err: must specify one of -l/--load, -w/--write\n");
-        free(path);
+    char *path = NULL;
+    if (prepare_args(argc, argv, &draw_border, &read, &write, path)) {
         return 1;
     }
 
@@ -69,10 +36,11 @@ int main(int argc, char* argv[]) {
         free(path);
         return 1;
     }
+
     if (fill_dungeon(&dungeon, ROOM_MIN_COUNT, ROOM_COUNT_MAX_RANDOMNESS, ROOM_MIN_WIDTH, ROOM_MIN_HEIGHT, ROOM_MAX_RANDOMNESS)) {
+        fprintf(stderr, "err: could not generate dungeon\n");
         dungeon_destroy(&dungeon);
         free(path);
-        fprintf(stderr, "err: could not generate dungeon\n");
         return 1;
     }
 
@@ -98,5 +66,59 @@ int main(int argc, char* argv[]) {
     dungeon_destroy(&dungeon);
     free(path);
 
+    return 0;
+}
+
+int prepare_args(int argc, char* argv[], int *draw_border, int *read, int *write, char *path) {
+    int i;
+    int path_next = 0;
+    char* home = getenv("HOME");
+    path = malloc(sizeof (char) * (strlen(home) + strlen(DEFAULT_PATH) + 1));
+    if (path == NULL) {
+        fprintf(stderr, "err: failed to allocate memory\n");
+        return 1;
+    }
+    strcpy(path, home);
+    strcat(path, DEFAULT_PATH);
+    for (i = 1; i < argc; i++) {
+        if (path_next) {
+            free(path);
+            // This is unnecessary, but simplifies all the free()s later.
+            path = malloc(sizeof (char) * (strlen(argv[i]) + 1));
+            if (path == NULL) {
+                fprintf(stderr, "err: failed to allocate memory\n");
+                return 1;
+            }
+            strcpy(path, argv[i]);
+            path_next = 0;
+        }
+        else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--load") == 0) *read = 1;
+        else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--save") == 0) *write = 1;
+        else if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--border") == 0) *draw_border = 1;
+        else if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--path") == 0) path_next = 1;
+        else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            printf("usage: %s [-lwbh] [-p <path>]\n  -l/--load: load a rungeon\n  -s/--save: write a random dungeon\n", argv[0]);
+            printf("  -b/--border: draw a border around the dungeon\n  -p/--path: override path to load/save ");
+            printf("(default ~/.cs327/dungeon)\n  -h/--help: display this message\n");
+            free(path);
+            return 0;
+        }
+        else {
+            fprintf(stderr, "err: unrecognized argument: %s. run -h/--help for usage\n", argv[i]);
+            free(path);
+            return 1;
+        }
+    }
+    if (path_next) {
+        fprintf(stderr, "err: specify a path after -p/--path\n");
+        free(path);
+        return 1;
+    }
+
+    if ((!*read && !*write) || (*read && *write)) {
+        fprintf(stderr, "err: must specify one of -l/--load, -w/--write\n");
+        free(path);
+        return 1;
+    }
     return 0;
 }
